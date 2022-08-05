05a060297bead73cd2492fd7271f3bab3a64011a
Michael Koulouvaris <mikekoul@gmail.com>
2022-08-05 17:43:11 -0600

    Add link on bulk discount index page to create new discount

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -53,5 +53,18 @@ RSpec.describe 'bulk discounts index page' do
         expect(page).to have_link("30% off Info Page")
       end
     end
+
+    it 'each bulk discount has a link to its show page' do
+      merchant_1 = Merchant.create!(name: 'Spongebob The Merchant')
+      merchant_2 = Merchant.create!(name: 'Sandy The Squirrel Merchant')
+      discount_10 = BulkDiscount.create!(name: "10% off", threshold: 10, percent_discount: 10, merchant_id: merchant_1.id)
+      discount_20 = BulkDiscount.create!(name: "20% off", threshold: 20, percent_discount: 20, merchant_id: merchant_1.id)
+      discount_30 = BulkDiscount.create!(name: "30% off", threshold: 30, percent_discount: 30, merchant_id: merchant_1.id)
+      special_discount_10 = BulkDiscount.create!(name: "Special 10% off", threshold: 10, percent_discount: 10, merchant_id: merchant_2.id)
+
+      visit "/merchants/#{merchant_1.id}/bulk_discounts"
+
+      expect(page).to have_link("Create New Discount")
+    end
   end
 end
