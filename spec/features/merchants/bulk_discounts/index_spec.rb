94d1368be9ec8588d3dff0aac9c9477e8e3b6a73
Michael Koulouvaris <mikekoul@gmail.com>
2022-08-05 14:53:18 -0600

    feat/Add link to bulk discount show page next to each discount in the merchant discounts index

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -30,5 +30,28 @@ RSpec.describe 'bulk discounts index page' do
         expect(page).to_not have_content("20% off Purchase of 20 items")
       end
     end
+
+    it 'each bulk discount has a link to its show page' do
+      merchant_1 = Merchant.create!(name: 'Spongebob The Merchant')
+      merchant_2 = Merchant.create!(name: 'Sandy The Squirrel Merchant')
+      discount_10 = BulkDiscount.create!(name: "10% off", threshold: 10, percent_discount: 10, merchant_id: merchant_1.id)
+      discount_20 = BulkDiscount.create!(name: "20% off", threshold: 20, percent_discount: 20, merchant_id: merchant_1.id)
+      discount_30 = BulkDiscount.create!(name: "30% off", threshold: 30, percent_discount: 30, merchant_id: merchant_1.id)
+      special_discount_10 = BulkDiscount.create!(name: "Special 10% off", threshold: 10, percent_discount: 10, merchant_id: merchant_2.id)
+
+      visit "/merchants/#{merchant_1.id}/bulk_discounts"
+
+      within "#discounts-#{discount_10.id}" do
+        expect(page).to have_link("10% off Info Page")
+      end
+
+      within "#discounts-#{discount_20.id}" do
+        expect(page).to have_link("20% off Info Page")
+      end
+
+      within "#discounts-#{discount_30.id}" do
+        expect(page).to have_link("30% off Info Page")
+      end
+    end
   end
 end
